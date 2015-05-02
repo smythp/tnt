b85ff5fe23fd2fcfd52f86c907f3122de3a81dfb
smythp <patricksmyth01@gmail.com>
2015-05-02 13:27:59 -0400

    sec update

--- tbot.py
+++ tbot.py
@@ -5,16 +5,17 @@
 import tweepy
 import time
 import sys
+import keys
 
 # Set keys to variables
 # You get the keys by going to the twitter account manager
 # and creating a new app. You then have to set permissions to
 # read and write and create a token. Once you have your keys
 # copy them here.
-CONSUMER_KEY = 'VvIdgQtsgD9fRySXqkPq1Yddp'
-CONSUMER_SECRET = 'AOsU7WkWwKLVPaOmP8BZN56P0IbnyGCicCbWsGb9XZERGp8q5N'
-ACCESS_KEY = '3087994289-QzMLdi2wS8aYudyNTrBkfwM6Kpb0wc6JU1r9yOT'
-ACCESS_SECRET = 'gO0Zcey3eKvwUzeKAhA34tKUCsiSpo2mEqL94B7qVjROM'
+CONSUMER_KEY = keys.CONSUMER_KEY
+CONSUMER_SECRET = keys.CONSUMER_SECRET
+ACCESS_KEY = keys.ACCESS_KEY
+ACCESS_SECRET = keys.ACCESS_SECRET
 
 # The arg stuff is confusing but basically it means you set the
 # file that the tweets are coming from when you run the Python
